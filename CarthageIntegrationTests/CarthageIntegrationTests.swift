c047d5cf7617e2cd9b8a5cf47766178b1cafb3ba
Dmitry Preobrazhenskiy <d.preobrahzenskiy@gmail.com>
2019-05-13 14:36:38 +0300

    Add fastlane testing
    By using Carthage the unit testing target is now
    using Quick/Nimble to instantiate the SDK and makes sure it can be updated
    
    IOS-210

--- CarthageIntegrationTests/CarthageIntegrationTests.swift
+++ CarthageIntegrationTests/CarthageIntegrationTests.swift
@@ -1,27 +1,15 @@
-//
+import Quick
+import Nimble
+import SalemoveSDK
 
-import XCTest
-
-class CarthageIntegrationTests: XCTestCase {
-
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+class QuickNimbleCarthageSpec: QuickSpec {
+    override func spec() {
+        describe("SalemoveSDK") {
+            it("should configure AppToken") {
+                let token = "test"
+                expect {(try Salemove.sharedInstance.configure(appToken: token))}.toNot(throwError())
+                expect(Salemove.sharedInstance.appToken) == token
+            }
         }
     }
-
 }
